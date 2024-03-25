94d2013e56a693015764461d346925a450ae48fe
AdekunleM <adekunlemichael1319@gmail.com>
2024-03-25 12:52:45 +0100

    Removed request

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = require('./routers/userRout.js')
 const adminRouter = require("./routers/adminRouter.js")
 const cartRouter = require ("./routers/cartRoute.js")
 const purchaseHistoryRouter = require("./routers/purchaseHistoryRoute.js")
-const returnRouter = require("./routers/returnRequestRoutes.js")
+const returnRouter = require("./routers/returnRequestRoute.js")
 const favoriteRouter = require("./routers/favoriteRouters.js")
 const PORT = process.env.PORT
 
