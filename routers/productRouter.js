441f838aa6019f41ce71f8a20aff9ea673b9c8ca
davidtobe13 <davidtobe1999@gmail.com>
2024-03-28 14:18:11 +0100

    david

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,13 +1,15 @@
 const router = require("express").Router();
 
 const { createProduct, updateProduct, getAllProducts, getProductById, deleteProduct } = require("../controllers/productController");
+const { authenticateAdmin } = require("../middleWare/authentication");
+const upload = require('../utils/multer')
 
 
-router.post('/create-product', createProduct)
-router.put('/update-product', updateProduct)
+router.post('/products/:categoryId', upload.array('images', 5), authenticateAdmin, createProduct);
+router.put('/products/:id', upload.array('images', 5), authenticateAdmin, updateProduct);
 router.get('/get-products', getAllProducts)
 router.get("/get-one-product", getProductById)
-router.delete("/delete-product", deleteProduct)
+router.delete("/delete-product", authenticateAdmin, deleteProduct)
 
 
 module.exports = router
